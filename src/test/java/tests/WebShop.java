e74970aedb304816b5baf50d1380822357d8fda6
Tanuxa459 <kliminatatianayog@gmail.com>
2025-02-23 16:19:15 +0300

    Update WebShop.java

--- src/test/java/tests/WebShop.java
+++ src/test/java/tests/WebShop.java
@@ -20,7 +20,7 @@ import static specs.TestSpec.addResponseSpec;
 @Tag("ApiUiTests")
 public class WebShop extends TestBase {
 
-
+    @DisplayName("Тест на авторизацию")
     @Test
     void loginWithApiTest() {
 
@@ -40,7 +40,7 @@ public class WebShop extends TestBase {
                 $(".account").shouldHave(text(login)));
     }
 
-
+    @DisplayName("Тест на добаление товара в корзину")
     @Test
     void loginWithAddApiTest() {
 
@@ -70,7 +70,8 @@ public class WebShop extends TestBase {
                 $(".cart-qty").shouldHave(text(responceAdd.getUpdatetopcartsectionhtml())));
 
     }
-
+    
+    @DisplayName("Тест на уделения товара из корзины")
     @Test
     void loginDeleteProductTest() {
 
